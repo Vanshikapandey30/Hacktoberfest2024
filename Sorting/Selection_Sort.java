b2ee108acf5d63de29fb419761cba27efe6f97fd
Madhav Agarwal <136367379+madhavagarwal3012@users.noreply.github.com>
2024-10-03 00:02:44 +0530

    Updated Variable Naming For Better Understanding Of The Code

--- Sorting/Selection_Sort.java
+++ Sorting/Selection_Sort.java
@@ -23,28 +23,28 @@
 
 //Code
 public class Selection_Sort {
-    static void selectionSort(int[] arr) {
-        int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            int minIdx = i;
-            for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIdx]) {
-                    minIdx = j;
+    static void selectionSort(int[] array) {
+        int length = array.length;
+        for (int index = 0; index < length - 1; index++) {
+            int mininumIndex = index;
+            for (int subIndex = index + 1; subIndex < length; subIndex++) {
+                if (array[subIndex] < array[mininumIndex]) {
+                    minimumIndex = subIndex;
                 }
             }
             // Swap the found minimum element with the first element
-            int temp = arr[minIdx];
-            arr[minIdx] = arr[i];
-            arr[i] = temp;
+            int temp = array[minimumIndex];
+            array[minimumIndex] = array[index];
+            array[index] = temp;
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {64, 25, 12, 22, 11};
-        selectionSort(arr);
+        int[] array = {64, 25, 12, 22, 11};
+        selectionSort(array);
         System.out.println("Sorted array: ");
-        for (int i : arr) {
-            System.out.print(i + " ");
+        for (int index : array) {
+            System.out.print(index + " ");
         }
     }
 }
