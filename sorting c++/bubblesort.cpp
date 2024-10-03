90071d7316034f1ed6bbd7d6e126d822a5930a71
siddhant-2002 <siddeshmukh711@gmail.com>
2024-10-03 21:55:25 +0530

    sorting techniques added

---
+++ sorting c++/bubblesort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+void bubbleSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+void printArray(const std::vector<int>& arr) {
+    for (int num : arr) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
+    std::cout << "Unsorted array: ";
+    printArray(arr);
+
+    bubbleSort(arr);
+
+    std::cout << "Sorted array: ";
+    printArray(arr);
+
+    return 0;
+}
