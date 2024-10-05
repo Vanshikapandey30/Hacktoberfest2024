3a8d702f99cc1c727abc18a7f7677466e56498de
Akash Dhankar <akashdhankar.2023it@sece.ac.in>
2024-10-05 21:03:44 +0530

    MaxSubarrays.java

--- LeetCode_Concepts/Sliding Window/maxsubarrays.java
+++ LeetCode_Concepts/Sliding Window/maxsubarrays.java
@@ -1,33 +1,25 @@
 import java.util.*;
 
-class Solution 
-{
-    public List<Integer> maxOfSubarrays(List<Integer> arr, int n, int k) 
-    {
+class Solution {
+    public List<Integer> maxOfSubarrays(List<Integer> arr, int n, int k) {
         Deque<Integer> deque = new LinkedList<>();
         int windowStart = 0, windowEnd = 0;
         List<Integer> result = new ArrayList<>();
 
-        while (windowEnd < n) 
-        {
-            while (!deque.isEmpty() && deque.peekLast() < arr.get(windowEnd)) 
-            {
+        while (windowEnd < n) {
+            while (!deque.isEmpty() && deque.peekLast() < arr.get(windowEnd)) {
                 deque.pollLast();
             }
 
             deque.offerLast(arr.get(windowEnd));
-            if (windowEnd - windowStart + 1 < k) 
-            {
+            if (windowEnd - windowStart + 1 < k) {
                 windowEnd++;
-            } 
+            }
 
-            else if (windowEnd - windowStart + 1 == k) 
-            {
+            else if (windowEnd - windowStart + 1 == k) {
                 result.add(deque.peekFirst());
 
-                
-                if (deque.peekFirst().equals(arr.get(windowStart))) 
-                {
+                if (deque.peekFirst().equals(arr.get(windowStart))) {
                     deque.pollFirst();
                 }
 
