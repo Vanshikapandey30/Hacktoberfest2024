3bb15e9373f180dff66771d5b84bfd616b92a687
Akash Dhankar <akashdhankar.2023it@sece.ac.in>
2024-10-05 19:31:41 +0530

    Added this very interesting problem of maximum of all subarrays of size k , which uses a sliding window algorithm .

---
+++ LeetCode_Concepts/Sliding Window/subarraysmax.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution 
+{
+    public List<Integer> maxOfSubarrays(List<Integer> arr, int n, int k) 
+    {
+        Deque<Integer> deque = new LinkedList<>();
+        int windowStart = 0, windowEnd = 0;
+        List<Integer> result = new ArrayList<>();
+
+        while (windowEnd < n) 
+        {
+            while (!deque.isEmpty() && deque.peekLast() < arr.get(windowEnd)) 
+            {
+                deque.pollLast();
+            }
+
+            deque.offerLast(arr.get(windowEnd));
+            if (windowEnd - windowStart + 1 < k) 
+            {
+                windowEnd++;
+            } 
+
+            else if (windowEnd - windowStart + 1 == k) 
+            {
+                result.add(deque.peekFirst());
+
+                
+                if (deque.peekFirst().equals(arr.get(windowStart))) 
+                {
+                    deque.pollFirst();
+                }
+
+                windowStart++;
+                windowEnd++;
+            }
+        }
+        return result;
+    }
+}
