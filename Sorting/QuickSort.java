f3cc0615364bacda49b266acf51bed9aab499308
prathmesh khandare <81563330+prathameshkhandare@users.noreply.github.com>
2024-10-05 18:53:41 +0530

    Create QuickSort.java

---
+++ Sorting/QuickSort.java
@@ -0,0 +1,40 @@
+public class QuickSort {
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {10, 80, 30, 90, 40, 50, 70};
+        int n = arr.length;
+
+        quickSort(arr, 0, n - 1);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
+}
