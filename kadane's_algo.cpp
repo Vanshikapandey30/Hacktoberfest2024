33b4b762a5b11578bd2f915945bce1466a0922ca
sahid raza <114164879+sahid9954@users.noreply.github.com>
2024-10-08 00:23:41 +0530

    Create kadane's_algo.cpp
    i want to connect for hacktoberfest

---
+++ kadane's_algo.cpp
@@ -0,0 +1,28 @@
+// C++ Program for Maximum Subarray Sum using Kadane's Algorithm
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to find the maximum subarray sum
+int maxSubarraySum(vector<int> &arr) {
+    int res = arr[0];
+    int maxEnding = arr[0];
+
+    for (int i = 1; i < arr.size(); i++) {
+      
+        // Find the maximum sum ending at index i by either extending 
+        // the maximum sum subarray ending at index i - 1 or by
+        // starting a new subarray from index i
+        maxEnding = max(maxEnding + arr[i], arr[i]);
+      
+        // Update res if maximum subarray sum ending at index i > res
+        res = max(res, maxEnding);
+    }
+    return res;
+}
+
+int main() {
+    vector<int> arr = {2, 3, -8, 7, -1, 2, 3};
+    cout << maxSubarraySum(arr);
+    return 0;
+}
