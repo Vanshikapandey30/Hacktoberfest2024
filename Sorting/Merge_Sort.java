a74a6edcba79a806515b83a67594372756ad8012
Pratiksha Churya A <91747537+PratikshaChuryaA@users.noreply.github.com>
2024-10-08 15:26:58 +0530

    Create Merge_Sort.java

---
+++ Sorting/Merge_Sort.java
@@ -0,0 +1,39 @@
+public void mergeSort(int[] arr, int left, int right) {
+    if (left < right) {
+        int mid = (left + right) / 2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+public void merge(int[] arr, int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+
+    int[] leftArray = new int[n1];
+    int[] rightArray = new int[n2];
+
+    for (int i = 0; i < n1; i++) {
+        leftArray[i] = arr[left + i];
+    }
+    for (int i = 0; i < n2; i++) {
+        rightArray[i] = arr[mid + 1 + i];
+    }
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (leftArray[i] <= rightArray[j]) {
+            arr[k++] = leftArray[i++];
+        } else {
+            arr[k++] = rightArray[j++];
+        }
+    }
+
+    while (i < n1) {
+        arr[k++] = leftArray[i++];
+    }
+    while (j < n2) {
+        arr[k++] = rightArray[j++];
+    }
+}
