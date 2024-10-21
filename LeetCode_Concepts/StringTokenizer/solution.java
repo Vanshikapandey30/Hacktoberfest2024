615255bf216f624f5d2f9d30a20f203e9974de42
Satyam <satyam878@gmail.com>
2024-10-22 00:08:18 +0530

    Added a question which can be solved using StringTokenizer:

---
+++ LeetCode_Concepts/StringTokenizer/solution.java
@@ -0,0 +1,15 @@
+import java.util.StringTokenizer;
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        String str="";
+        if(s==null||s.length()==0)
+            return 0;
+        StringTokenizer st=new StringTokenizer(s);
+        int count=st.countTokens();
+        for(int i=0;i<count-1;i++)
+            str=st.nextToken();
+        str=st.nextToken();
+        return (int)(str.length());
+    }
+}
